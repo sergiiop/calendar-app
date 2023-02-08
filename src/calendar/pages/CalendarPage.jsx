5acf5a33c7f58808ddad8005e14778760cc3b0fb
Sergio Pérez <sergio-luis98@hotmail.com>
2023-02-07 23:21:03 -0500

    init

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours } from 'date-fns'
+
+import { Navbar, CalendarEvent, CalendarModal } from '../'
+import { localizer, getMessagesES } from '../../helpers'
+
+const events = [
+  {
+    title: 'Cumple del jefe',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fffaa',
+    user: {
+      _id: '123',
+      name: 'Fernando'
+    }
+  }
+]
+
+export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem('lastView') || 'week'
+  )
+  const eventStyleGetter = (event, start, env, isSelected) => {
+    console.log({ event, start, env, isSelected })
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+    return {
+      style
+    }
+  }
+
+  const onDoubleClick = (event) => {
+    console.log({ doubleClick: event })
+  }
+
+  const onSelect = (event) => {
+    console.log({ click: event })
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event)
+    setLastView(event)
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        defaultView={lastView}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
+      />
+      <CalendarModal />
+    </>
+  )
+}
